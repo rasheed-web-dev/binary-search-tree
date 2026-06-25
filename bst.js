29970f4510999a31a5503577fb54198700f39a86
rasheed-web-dev <274648074+rasheed-web-dev@users.noreply.github.com>
2026-06-26 01:23:33 +0300

    Create Tree class and make buildtree function

--- bst.js
+++ bst.js
@@ -1,4 +1,4 @@
-class node {
+class TreeNode {
   constructor(value, left = null, right = null) {
     this.value = value;
     this.left = left;
@@ -13,3 +13,32 @@ class node {
     this.right = right;
   }
 }
+
+class Tree {
+  constructor(arr) {
+    arr.sort((a, b) => a - b);
+    let newArr = [];
+    arr.forEach((element) => {
+      if (!newArr.includes(element)) {
+        newArr.push(element);
+      }
+    });
+    this.arr = newArr;
+    this.root = this.buildTree(this.arr);
+  }
+
+  buildTree(array) {
+    if (array.length <= 1) {
+      return new TreeNode(array[0]);
+    }
+    const midIndex = Math.floor(array.length / 2);
+    const midElement = array[midIndex];
+    const root = new TreeNode(midElement);
+    root.setLeft(this.buildTree(array.slice(0, midIndex)));
+    root.setRight(this.buildTree(array.slice(midIndex + 1)));
+    return root;
+  }
+}
+
+const t = new Tree([]);
+console.log(t.root);
