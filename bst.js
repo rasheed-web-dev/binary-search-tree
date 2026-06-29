6a56eca865679c83dd04c535d786c31e222597ee
rasheed-web-dev <274648074+rasheed-web-dev@users.noreply.github.com>
2026-06-29 03:39:21 +0300

    Make insert function

--- bst.js
+++ bst.js
@@ -61,9 +61,44 @@ class Tree {
       this.includes(value, rootNode.right)
     );
   }
+
+  insert(value, rootNode = this.root) {
+    if (rootNode == null) {
+      const root = new TreeNode(value);
+      return root;
+    }
+    if (rootNode.value == value) {
+      return rootNode;
+    }
+    if (rootNode.value > value) {
+      rootNode.left = this.insert(value, rootNode.left);
+      return rootNode;
+    } else {
+      rootNode.right = this.insert(value, rootNode.right);
+      return rootNode;
+    }
+  }
 }
 
 const t = new Tree([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log(t.root);
+t.insert(10);
+t.insert(-5);
+t.insert(0);
+t.insert(3.14);
+t.insert(-8);
+t.insert(42);
+t.insert(7);
+t.insert(-1);
+t.insert(100);
+t.insert(-50);
+t.insert(0.001);
+t.insert(-0.5);
+t.insert(999);
+t.insert(-999);
+t.insert(20);
+t.insert(15);
+t.insert(-20);
+t.insert(33);
+t.insert(-33);
 t.prettyPrint(t.root);
-console.log(t.includes(0));
